2cd19b395f4320330b66dff1ef98d149f3a40a31
Martin Burchell <martinb@aptivate.org>
2016-06-24 14:48:24 +0100

    Add test for notify dataset/update

--- ckanext/syndicate/tests/test_plugin.py
+++ ckanext/syndicate/tests/test_plugin.py
@@ -8,13 +8,22 @@ from ckan.model.domain_object import DomainObjectOperation
 from ckanext.syndicate.plugin import SyndicatePlugin
 
 
-class TestPlugin(unittest.TestCase):
-    def test_notify_syndicates_task(self):
-        entity = model.Package()
-        entity.extras = {'syndicate': 'true'}
+class TestNotify(unittest.TestCase):
+    def setUp(self):
+        super(TestNotify, self).setUp()
+        self.entity = model.Package()
+        self.entity.extras = {'syndicate': 'true'}
+        self.syndicate_patch = patch('ckanext.syndicate.plugin.syndicate_task')
+        self.plugin = SyndicatePlugin()
 
-        with patch('ckanext.syndicate.plugin.syndicate_task') as mock_syndicate:
-            plugin = SyndicatePlugin()
+    def test_syndicates_task_for_dataset_create(self):
+        with self.syndicate_patch as mock_syndicate:
+            self.plugin.notify(self.entity, DomainObjectOperation.new)
+            mock_syndicate.assert_called_with(self.entity.id,
+                                              'dataset/create')
 
-            plugin.notify(entity, DomainObjectOperation.new)
-            mock_syndicate.assert_called_with(entity.id, 'dataset/create')
+    def test_syndicates_task_for_dataset_update(self):
+        with self.syndicate_patch as mock_syndicate:
+            self.plugin.notify(self.entity, DomainObjectOperation.changed)
+            mock_syndicate.assert_called_with(self.entity.id,
+                                              'dataset/update')
