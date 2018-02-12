2cf34e18d182bd0ab77d3b89dcc9df5a73afcfee
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2018-02-12 15:13:48 +0200

    forcely sindicate from cli

--- ckanext/syndicate/command.py
+++ ckanext/syndicate/command.py
@@ -57,11 +57,15 @@ class SyndicateCommand(CkanCommand):
 
     def _sync(self):
         plugin = get_plugin('syndicate')
+        from ckanext.syndicate.plugin import get_syndicate_flag
+
         packages = ckan_model.Session.query(ckan_model.Package).filter_by(
             state='active')
         if len(self.args) > 1:
             packages = [ckan_model.Package.get(self.args[1])]
+
         for package in packages:
             sleep(0.02)
+            package.extras[get_syndicate_flag()] = 'true'
             print('Sending syndication signal to {}'.format(package.id))
             plugin.notify(package, 'changed')
