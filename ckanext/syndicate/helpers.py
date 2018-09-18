6c8e4f4792a4d8e2f6b5ac264534e2840cec2b9c
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2018-09-18 14:34:51 +0300

    add owner org predicates

---
+++ ckanext/syndicate/helpers.py
@@ -0,0 +1,19 @@
+try:
+    from ckan.common import config
+except ImportError:
+    from pylons import config
+
+CONFIG_ALLOWED_ORG = 'ckanext.syndication.predicate.allowed_organization'
+CONFIG_DENIED_ORG = 'ckanext.syndication.predicate.denied_organization'
+
+
+def organization_owns_dataset(pkg, conf_name=CONFIG_ALLOWED_ORG):
+    orgs = pkg.get_groups('organization')
+    if not orgs:
+        return False
+    title = orgs[0].title
+    return title == config.get(conf_name)
+
+
+def organization_not_owns_dataset(pkg):
+    return not organization_owns_dataset(pkg, CONFIG_DENIED_ORG)
